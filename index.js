968ffe584f01a349ec97390bfe28081a4fa7acc0
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2024-12-01 00:44:07 -0500

    Esto es una resta

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const asd = () => {
-    return "Esto es JavaScript con editor";
+  const a = 1;
+  const b = 2;
+  const c = 3;
+  const resta = a - b - c;
+  return "Esto es JavaScript con editor. Y la resta es: " + resta;
 };
